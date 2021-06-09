318a587f2ae69e293ea0a35df913fd9e4ca88f3c
RMYanenko <yanenko.r.m@mail.com>
2021-06-09 23:36:50 +0300

    reate first component htag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
-import "../styles/globals.css";
+import "../styles/globals.scss";
 import { AppProps } from "next/dist/next-server/lib/router/router";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Hello World</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
