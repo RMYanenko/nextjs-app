fb5a723f87a7b94abdd436b9ced0767dee2fb904
RMYanenko <yanenko.r.m@gmail.com>
2021-06-13 23:59:34 +0300

    add c. Rating

---
+++ components/Rating/Rating.tsx
@@ -0,0 +1,72 @@
+import { useEffect, useState, KeyboardEvent } from "react";
+import { RatingProps } from "./Rating.props";
+import s from "./Rating.module.scss";
+import cn from "classnames";
+import StarIcon from "./star.svg";
+
+export const Rating = ({
+  isEditable = false,
+  rating,
+  setRating,
+  ...props
+}: RatingProps): JSX.Element => {
+  const [ratingArray, setRatingArray] = useState<JSX.Element[]>(
+    new Array(5).fill(<></>)
+  );
+
+  useEffect(() => {
+    constructRating(rating);
+  }, [rating]);
+
+  const constructRating = (currentRating: number) => {
+    const updateArray = ratingArray.map((r: JSX.Element, i: number) => {
+      return (
+        <span
+          className={cn(s.star, {
+            [s.filled]: i < currentRating,
+            [s.editable]: isEditable,
+          })}
+          onMouseEnter={() => changeDisplay(i + 1)}
+          onMouseLeave={() => changeDisplay(rating)}
+          onClick={() => onClick(i + 1)}
+        >
+          <StarIcon
+            tabIndex={isEditable ? 0 : -1}
+            onKeyDown={(e: KeyboardEvent<SVGSVGElement>) =>
+              isEditable && handleSpace(i + 1, e)
+            }
+          />
+        </span>
+      );
+    });
+    setRatingArray(updateArray);
+  };
+
+  const changeDisplay = (i: number) => {
+    if (!isEditable) {
+      return;
+    }
+    constructRating(i);
+  };
+  const onClick = (i: number) => {
+    if (!isEditable || !setRating) {
+      return;
+    }
+    setRating(i);
+  };
+
+  const handleSpace = (i: number, e: KeyboardEvent<SVGAElement>) => {
+    if (e.code != "Space" || !setRating) {
+      return;
+    }
+    setRating(i);
+  };
+
+  return (
+    <div {...props}>
+      {ratingArray.map((r, i) => (
+        <span key={i}>{r}</span>
+      ))}
+    </div>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,38 @@
-import React from "react";
-import { Button, Htag, Paragraph, Tag } from "../components";
+import React, { useState } from "react";
+import { Button, Htag, Paragraph, Rating, Tag } from "../components";
 
 export default function Home(): JSX.Element {
+
+  const [rating, setRating] = useState<number>(4);
+
   return (
     <>
       <Htag tag="h1">Text</Htag>
-      <Button appearance="primary" arrow="right">Клик</Button>
-      <Button appearance="ghost" arrow="right">Клик</Button>
-      <Paragraph size="s">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus atque nam iusto perspiciatis cum consequuntur perferendis, autem quibusdam, rem, nesciunt voluptates rerum maxime aliquam. Numquam dignissimos quod similique minima labore?</Paragraph>
-      <Paragraph size="m">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus atque nam iusto perspiciatis cum consequuntur perferendis, autem quibusdam, rem, nesciunt voluptates rerum maxime aliquam. Numquam dignissimos quod similique minima labore?</Paragraph>
-      <Paragraph size="xl">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus atque nam iusto perspiciatis cum consequuntur perferendis, autem quibusdam, rem, nesciunt voluptates rerum maxime aliquam. Numquam dignissimos quod similique minima labore?</Paragraph>
-      <Tag size='s' color="red">!!!</Tag>
+      <Button
+        appearance="primary"
+        arrow="right"
+      >
+        Клик
+      </Button>
+      <Button
+        appearance="ghost"
+        arrow="right"
+      >
+        Клик
+      </Button>
+      <Paragraph size="s">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus atque
+        nam iusto perspiciatis cum consequuntur perferendis, autem quibusdam,
+        rem, nesciunt voluptates rerum maxime aliquam. Numquam dignissimos quod
+        similique minima labore?
+      </Paragraph>
+      <Tag size="s" color="red">
+        !!!
+      </Tag>
+      <Tag size='m'>
+        !!!
+      </Tag>
+      <Rating rating={4}  isEditable setRating={setRating} />
     </>
   );
 }
