b6d54f6af150b7a8b218558fe0565abbdb27c78b
RMYanenko <yanenko.r.m@mail.com>
2021-06-13 12:15:33 +0300

    create component BTN

---
+++ components/Button/Button.tsx
@@ -0,0 +1,34 @@
+import { ButtonProps } from "./Button.props";
+import s from "./Button.module.scss";
+import ArrowIcon from "./arrow.svg";
+import cn from "classnames";
+
+export default function Button({
+  appearance,
+  arrow = "none",
+  children,
+  className,
+  ...props
+}: ButtonProps): JSX.Element {
+  return (
+    <button
+      className={cn(s.btn, className, {
+        [s.primary]: appearance == "primary",
+        [s.ghost]: appearance == "ghost",
+      })}
+      {...props}
+    >
+      {children}
+      {arrow != "none" && (
+        <span
+          className={cn(s.arrow, {
+            [s.right]: arrow == "right",
+            [s.down]: arrow == "down",
+          })}
+        >
+          <ArrowIcon />
+        </span>
+      )}
+    </button>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Htag } from "../components";
+import Button from "../components/Button/Button";
 
 export default function Home(): JSX.Element {
   return (
     <>
       <Htag tag="h1">Text</Htag>
+      <Button appearance="primary" arrow="right">Клик</Button>
+      <Button appearance="ghost" arrow="right">Клик</Button>
     </>
   );
 }
